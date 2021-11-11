f8da87497d7b5ca37bf317bc612ac7ab00a16ec4
Ozzy Cohen Abramson <odentionofficial@gmail.com>
2021-11-11 18:30:51 +0200

    fixed position of intro and menu hamburger

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,28 +2,26 @@ import React from "react";
 
 export default function Experience() {
   return (
-    <div className="row">
-      <div className="experience tab-content active">
-        <div className="row">
-          <div className="timeline">
-            <div className="row">
-              <div className="timeline-item">
-                <div className="timeline-item-inner outer-shadow">
-                  <i className="fas fa-briefcase icon"></i>
-                  <span>Date</span>
+    <div className='row'>
+      <div className='experience tab-content active'>
+        <div className='row'>
+          <div className='timeline'>
+            <div className='row'>
+              <div className='timeline-item'>
+                <div className='timeline-item-inner outer-shadow'>
+                  <i className='fas fa-briefcase icon'></i>
+                  <span>October, 2021 - present</span>
                   <h3>Work Description</h3>
-                  <h4>Company name, Israel</h4>
+                  <h4>Fitback, Israel</h4>
                   <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Mollitia quidem ipsa et inventore, vel harum, deleniti,
-                    eveniet sunt perferendis ab earum! Laudantium totam saepe,
-                    atque necessitatibus velit illum maiores quam. ipsum dolor
-                    sit amet consectetur adipisicing elit.
+                    Developing a new fitness technology, useng ML in Python. I
+                    works both on the Fe and the Be as I write the
+                    funcitonallity for the exercises.
                   </p>
                 </div>
               </div>
 
-              <div className="timeline-item">
+              {/*<div className="timeline-item">
                 <div className="timeline-item-inner outer-shadow">
                   <i className="fas fa-briefcase icon"></i>
                   <span>Date</span>
@@ -39,7 +37,7 @@ export default function Experience() {
                 </div>
               </div>
 
-              <div className="timeline-item">
+               <div className="timeline-item">
                 <div className="timeline-item-inner outer-shadow">
                   <i className="fas fa-briefcase icon"></i>
                   <span>Date</span>
@@ -53,7 +51,7 @@ export default function Experience() {
                     sit amet consectetur adipisicing elit.
                   </p>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
