4fd83d599d9eac45a660c2a4fad7663467ba3df2
Ozzy Cohen Abramson <odentionofficial@gmail.com>
2024-12-16 20:28:28 +0200

    add: allaborate on each job desctiption

--- src/components/Experience/experienceList.js
+++ src/components/Experience/experienceList.js
@@ -4,11 +4,11 @@ export default [
     company: "Fincom, Israel",
     dates: "March 2023 - Present",
     description: `
-    I contribute to the development of cutting-edge anti-money laundering (AML) solutions tailored for banks and financial institutions. <br />
-    Leveraging <strong>Vue 3</strong>, I design and implement intuitive user interfaces that enable financial institutions to manage transactions across multiple payment types, including Wire, RTP, ACH, and SEPA. <br />
-    I design and implement robust microservices using <strong>Node.js</strong> (<strong>Express</strong>) and <strong>Golang</strong> (<strong>GIN</strong>), focusing on user authentication, transaction management, and message-driven architectures with <strong>RabbitMQ</strong>. <br />    
-    I work extensively with <strong>MongoDB</strong> to manage and query data, ensuring secure and efficient database operations. <br />
-    I streamline development workflows by deploying services on <strong>Azure</strong> using <strong>GitLab CI/CD</strong> pipelines, ensuring a seamless and efficient developer experience.
+      I contribute to the development of cutting-edge anti-money laundering (AML) solutions tailored for banks and financial institutions. <br />
+      Leveraging <b style="font-weight: 800">Vue 3</b>, I design and implement intuitive user interfaces that enable financial institutions to manage transactions across multiple payment types, including Wire, RTP, ACH, and SEPA. <br />
+      I design and implement robust microservices using <b style="font-weight: 800">Node.js</b> (<b style="font-weight: 800">Express</b>) and <b style="font-weight: 800">Golang</b> (<b style="font-weight: 800">GIN</b>), focusing on user authentication, transaction management, and message-driven architectures with <b style="font-weight: 800">RabbitMQ</b>. <br />    
+      I work extensively with <b style="font-weight: 800">MongoDB</b> to manage and query data, ensuring secure and efficient database operations. <br />
+      I streamline development workflows by deploying services on <b style="font-weight: 800">Azure</b> using <b style="font-weight: 800">GitLab CI/CD</b> pipelines, ensuring a seamless and efficient developer experience.
     `,
   },
   {
@@ -16,41 +16,33 @@ export default [
     company: "Synamedia, Israel",
     dates: "February 2022 - March 2023",
     description: `
-            Working with Python and node.js (express, nest) in backend
-            to build micro-services components. <br /> React in the
-            frontend side. <br />
-            Working with Docker, AWS and github actions for deployment.
-        `,
+      Developed microservices using 
+      <b style="font-weight: 800">Python and node.js </b>with frameworks like <b style="font-weight: 800">Express and NestJS</b>, 
+      focusing on modular and maintainable codebases. <br />
+      Built dynamic and responsive user interfaces using <b style="font-weight: 800">React</b>, 
+      ensuring seamless interaction with backend services. <br />
+      Streamlined deployments with <b style="font-weight: 800">Docker</b> containers, utilized <b style="font-weight: 800">AWS</b> for scalable cloud infrastructure, 
+      and implemented CI/CD pipelines using <b style="font-weight: 800">GitHub Actions</b> for efficient, automated workflows.
+    `,
   },
   {
     title: "Frontend lecturer",
     company: "ITC, Israel",
     dates: "January 2023",
     description: `
-    Introducing the frontend side of fullstack development to
-    students in fullstack course. Teaching HTML, CSS and
-    Javascript as well as React.js
+      I introduced students to the <b style="font-weight: 800">frontend side of full-stack development</b>, equipping them with essential skills for building modern web applications. <br />
+      Taught foundational web development technologies, including <b style="font-weight: 800">HTML, CSS, and JavaScript </b>, to lay a strong groundwork for creating dynamic and responsive web interfaces.<br />
+      Provided in-depth training on <b style="font-weight: 800">React.js </b>, covering component-based architecture, state management, and building interactive user experiences.<br />
     `,
   },
   {
     title: "Lecturer",
     company: "ITC, Israel",
-    dates: "January 2023 - Augost 2023",
+    dates: "January 2023 - August 2023",
     description: `
-        Introducing the world of Python to Cyber Security and Data
-        Analyst students. <br /> Teaching the fundamentals of
-        Front-end development in Full-stack programs (HTML, CSS and
-        JS Basics).
-    `,
-  },
-  {
-    title: "Python lecturer",
-    company: "ITC, Israel",
-    dates: "December 2021",
-    description: `
-        Introducing the world of Python to Cyber Security Analyst
-        Students. Teaching the fundamentals of Python, building and
-        checking exercises.
+      I introduced aspiring Cyber Security Analysts to the world of <b style="font-weight: 800">Python</b>, laying a solid foundation for their programming skills.<br />
+      Taught Python fundamentals, including <b style="font-weight: 800">data structures, algorithms, and problem-solving techniques </b>.<br />
+      Designed and evaluated exercises to reinforce key concepts and enhance practical coding abilities.<br />
     `,
   },
   {
@@ -58,10 +50,9 @@ export default [
     company: "Fitback, Israel",
     dates: "October 2021 - December 2021",
     description: `
-        Developing real-time workout feedback for tracking the
-        user’s movements and exercises, using ML in Python / JS. I
-        work both on the Front and Back End as I write the
-        funcitonallity for the exercises.
+      Contributed to the development of real-time workout feedback systems that track user movements and exercises, utilizing 
+      <b style="font-weight: 800">Machine Learning (ML) in Python and JavaScript</b>. <br />
+      I contributed to both the frontend and backend, ensuring smooth integration of the exercise tracking functionality within the overall system.
     `,
   },
 ];
