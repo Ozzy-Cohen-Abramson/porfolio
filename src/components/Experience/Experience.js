45ddb0b5bb431d7e9b522067a23342381829d79f
Ozzy Cohen Abramson <odentionofficial@gmail.com>
2024-12-16 16:48:50 +0200

    add: move experience to a different file

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,4 +1,5 @@
 import React from "react";
+import experienceList from "./experienceList";
 
 export default function Experience() {
   return (
@@ -7,74 +8,20 @@ export default function Experience() {
         <div className='row'>
           <div className='timeline'>
             <div className='row'>
-              <div className='timeline-item'>
-                <div className='timeline-item-inner outer-shadow'>
-                  <i className='fas fa-briefcase icon'></i>
-                  <span>February 2022 - Present</span>
-                  <h3>Software associate engineer</h3>
-                  <h4>Synamedia, Israel</h4>
-                  <p>
-                    Working with Python and node.js (express, nest) in backend
-                    to build micro-services components. <br /> React in the
-                    frontend side. <br />
-                    Working with Docker, AWS and github actions for deployment.
-                  </p>
+              {experienceList.map((experience, index) => (
+                <div className='timeline-item' key={index}>
+                  <div className='timeline-item-inner outer-shadow'>
+                    <i className='fas fa-briefcase icon'></i>
+                    <span>{experience.dates}</span>
+                    <h3>{experience.title}</h3>
+                    <h4>{experience.company}</h4>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: experience.description,
+                      }}></p>
+                  </div>
                 </div>
-              </div>
-              <div className='timeline-item'>
-                <div className='timeline-item-inner outer-shadow'>
-                  <i className='fas fa-briefcase icon'></i>
-                  <span>January 2023 - present</span>
-                  <h3>Lecturer</h3>
-                  <h4>ITC, Israel</h4>
-                  <p>
-                    Introducing the world of Python to Cyber Security and Data
-                    Analyst students. <br /> Teaching the fundamentals of
-                    Front-end development in Full-stack programs (HTML, CSS and
-                    JS Basics).
-                  </p>
-                </div>
-              </div>
-              <div className='timeline-item'>
-                <div className='timeline-item-inner outer-shadow'>
-                  <i className='fas fa-briefcase icon'></i>
-                  <span>January 2023</span>
-                  <h3>Frontend lecturer</h3>
-                  <h4>ITC, Israel</h4>
-                  <p>
-                    Introducing the frontend side of fullstack development to
-                    students in fullstack course. Teaching HTML, CSS and
-                    Javascript as well as React.js
-                  </p>
-                </div>
-              </div>
-              <div className='timeline-item'>
-                <div className='timeline-item-inner outer-shadow'>
-                  <i className='fas fa-briefcase icon'></i>
-                  <span>December 2021</span>
-                  <h3>Python lecturer</h3>
-                  <h4>ITC, Israel</h4>
-                  <p>
-                    Introducing the world of Python to Cyber Security Analyst
-                    Students. Teaching the fundamentals of Python, building and
-                    checking exercises.
-                  </p>
-                </div>
-              </div>
-              <div className='timeline-item'>
-                <div className='timeline-item-inner outer-shadow'>
-                  <i className='fas fa-briefcase icon'></i>
-                  <span>October 2021 - December 2021</span>
-                  <h3>Full Stack Developer</h3>
-                  <h4>Fitback, Israel</h4>
-                  <p>
-                    Developing real-time workout feedback for tracking the
-                    user’s movements and exercises, using ML in Python / JS. I
-                    work both on the Front and Back End as I write the
-                    funcitonallity for the exercises.
-                  </p>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
---
+++ src/components/Experience/experienceList.js
@@ -0,0 +1,67 @@
+export default [
+  {
+    title: "Full stack engineer",
+    company: "Fincom, Israel",
+    dates: "March 2023 - Present",
+    description: `
+    I contribute to the development of cutting-edge anti-money laundering (AML) solutions tailored for banks and financial institutions. <br />
+    Leveraging <strong>Vue 3</strong>, I design and implement intuitive user interfaces that enable financial institutions to manage transactions across multiple payment types, including Wire, RTP, ACH, and SEPA. <br />
+    I design and implement robust microservices using <strong>Node.js</strong> (<strong>Express</strong>) and <strong>Golang</strong> (<strong>GIN</strong>), focusing on user authentication, transaction management, and message-driven architectures with <strong>RabbitMQ</strong>. <br />    
+    I work extensively with <strong>MongoDB</strong> to manage and query data, ensuring secure and efficient database operations. <br />
+    I streamline development workflows by deploying services on <strong>Azure</strong> using <strong>GitLab CI/CD</strong> pipelines, ensuring a seamless and efficient developer experience.
+    `,
+  },
+  {
+    title: "Software associate engineer",
+    company: "Synamedia, Israel",
+    dates: "February 2022 - March 2023",
+    description: `
+            Working with Python and node.js (express, nest) in backend
+            to build micro-services components. <br /> React in the
+            frontend side. <br />
+            Working with Docker, AWS and github actions for deployment.
+        `,
+  },
+  {
+    title: "Frontend lecturer",
+    company: "ITC, Israel",
+    dates: "January 2023",
+    description: `
+    Introducing the frontend side of fullstack development to
+    students in fullstack course. Teaching HTML, CSS and
+    Javascript as well as React.js
+    `,
+  },
+  {
+    title: "Lecturer",
+    company: "ITC, Israel",
+    dates: "January 2023 - Augost 2023",
+    description: `
+        Introducing the world of Python to Cyber Security and Data
+        Analyst students. <br /> Teaching the fundamentals of
+        Front-end development in Full-stack programs (HTML, CSS and
+        JS Basics).
+    `,
+  },
+  {
+    title: "Python lecturer",
+    company: "ITC, Israel",
+    dates: "December 2021",
+    description: `
+        Introducing the world of Python to Cyber Security Analyst
+        Students. Teaching the fundamentals of Python, building and
+        checking exercises.
+    `,
+  },
+  {
+    title: "Full Stack Developer",
+    company: "Fitback, Israel",
+    dates: "October 2021 - December 2021",
+    description: `
+        Developing real-time workout feedback for tracking the
+        user’s movements and exercises, using ML in Python / JS. I
+        work both on the Front and Back End as I write the
+        funcitonallity for the exercises.
+    `,
+  },
+];
